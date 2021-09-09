f8a6033c18630045d3b8eb1ab07c7a2134c9011d
BAGZZlash@gmx.de <BAGZZlash@gmx.de>
2021-09-09 16:17:15 +0200

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string Username = Console.ReadLine();
+
+            var input = new HelloRequest { Name = Username };
+
+            Console.WriteLine("\nOpening channel.");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine("Sending greeter request.");
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine("\nServer replied:");
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
